c9a89bfaa5d609744bf1a996184418b9a8f8f3c0
Thales-Eduardo <80049862+Thales-Eduardo@users.noreply.github.com>
2021-04-03 12:19:45 -0300

    Update 01.js

--- 04-objectES678/01.js
+++ 04-objectES678/01.js
@@ -1,20 +1,22 @@
-function extrairValores(extrair) {
-    return extrair.forEach(item => Object.values(item))
-}
+var library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true,
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true,
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
 
-console.log(extrairValores([
-    {
-        author: 'Bill Gates',
-        title: 'The Road Ahead',
-        readingStatus: true
-    },
-    {
-        author: 'Steve Jobs',
-        title: 'Walter Isaacson',
-        readingStatus: true
-    },
-    {
-        author: 'Suzanne Collins',
-        title: 'Mockingjay: The Final Book of The Hunger Games',
-        readingStatus: false
-}]))
+const result = library.forEach((books) => {
+  const { author, title, readingStatus } = books;
+  if (readingStatus) console.log({ author, title, readingStatus });
+});
