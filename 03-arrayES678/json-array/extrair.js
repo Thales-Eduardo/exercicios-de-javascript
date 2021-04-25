f6f86f01a442d3270798470492fce3eab997db5e
thales eduardo <thalesdev22@gmailcom>
2021-04-25 11:02:45 -0300

    add exer

--- 03-arrayES678/json-array/extrair.js
+++ 03-arrayES678/json-array/extrair.js
@@ -1,35 +1,59 @@
 const dados = require("./dados.json");
 function extrair(propriedade) {
   let valores = propriedade.transactions;
-
-  let income = valores
-    .map((valor) => {
-      if (valor.type === "income") {
-        return valor.value;
-      } else {
-        return 0;
+  const { income, outcome } = valores.reduce(
+    (acc, transaction) => {
+      switch (transaction.type) {
+        case "income":
+          acc.income += transaction.value;
+          break;
+        case "outcome":
+          acc.outcome += transaction.value;
+          break;
+        default:
+          break;
       }
-    })
-    .reduce((acc, vall) => {
-      return acc + vall;
-    });
-  let outcome = valores
-    .map((valor) => {
-      if (valor.type === "outcome") {
-        return valor.value;
-      } else {
-        return 0;
-      }
-    })
-    .reduce((acc, vall) => {
-      return acc + vall;
-    });
-  return JSON.stringify({
-    balance: {
-      income: `${income}`,
-      outcome: `${outcome}`,
-      total: `${income - outcome}`,
+      return acc;
     },
-  });
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+  const total = income - outcome;
+  return { valores, income, outcome, total };
 }
 console.log(extrair(dados));
+
+//ou assim
+
+// let income = valores
+//     .map((valor) => {
+//       if (valor.type === "income") {
+//         return valor.value;
+//       } else {
+//         return 0;
+//       }
+//     })
+//     .reduce((acc, vall) => {
+//       return acc + vall;
+//     });
+//   let outcome = valores
+//     .map((valor) => {
+//       if (valor.type === "outcome") {
+//         return valor.value;
+//       } else {
+//         return 0;
+//       }
+//     })
+//     .reduce((acc, vall) => {
+//       return acc + vall;
+//     });
+//   return JSON.stringify({
+//     balance: {
+//       income: `${income}`,
+//       outcome: `${outcome}`,
+//       total: `${income - outcome}`,
+//     },
+//   });
